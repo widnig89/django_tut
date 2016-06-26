33197698438f6e4b9184dd37d3fb3eaa87f43fda
Dominik Widnig <dwidnig@student.tugraz.at>
2016-06-26 17:05:36 +0200

    cont. with tut 6 and 7

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Basic information', {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
